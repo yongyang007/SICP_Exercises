a7a2273e00923261dcc267dd05c90c7de794d386
yongyang007 <yongyang007@gmail.com>
2015-11-27 18:45:06 +0900

    add another newton_s_method_square_root with internal definition

---
+++ chapter_1/newton_s_method_square_root_internal_definition.scm
@@ -0,0 +1,15 @@
+(define (square x) (* x x))
+
+(define (sqrt x)
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (average x y)
+    (/ (+ x y) 2))
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+
+  (sqrt-iter 1.0))
