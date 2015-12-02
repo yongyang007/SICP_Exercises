9ca0072ab276ddd08e3c2f5eab07199aa67084e7
yongyang007 <yongyang007@gmail.com>
2015-12-02 14:18:42 +0900

    add count_change

---
+++ chapter_1/count_change.scm
@@ -0,0 +1,19 @@
+                                        ;换零钱问题：给了半美元 四分之一美元 10美分 5美分和1美分的硬币，统计出将给定现金数换成这些零钱的方式的种数
+(define (count-change amount)
+  (cc amount 5))
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+(count-change 100)
