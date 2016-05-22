81e9447b62120192eedc93024a0355e3549697e6
YangYong <yongyang007@gmail.com>
2016-05-23 00:26:48 +0900

    add exercise 2.33 2.34 2.35 2.36

---
+++ chapter_2/section_2_2/exercise_2_33.scm
@@ -0,0 +1,13 @@
+(load "../../tool/accumulate.scm")
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (append (list (p x)) y)) '() sequence))
+(map (lambda (x) (+ x 1)) (list 1 2 3 4 5))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+(append (list 1 2 3) (list 4 5 6 7))
+
+(define (length sequence)
+  (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
+(length (list 1 2 3 4 5 6))
---
+++ chapter_2/section_2_2/exercise_2_35.scm
@@ -0,0 +1,12 @@
+(load "../../tool/accumulate.scm")
+
+(define (count-leaves t)
+  (accumulate +
+              0
+              (map (lambda (x)
+                     (cond ((null? x) 0)
+                           ((not (pair? x)) 1)
+                           (else (count-leaves x))))
+                   t)))
+
+(count-leaves (list 1 (list 2 3 4) 5 (list 6 (list 7 8 (list 9)) 10) 11))
