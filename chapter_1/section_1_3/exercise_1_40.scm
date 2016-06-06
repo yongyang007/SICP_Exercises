613d101f73475b9f7bc6bc3f21dad4472de07dba
yongyang007 <yongyang007@gmail.com>
2016-06-06 12:59:36 +0900

    readd exercise 1.40

--- chapter_1/section_1_3/exercise_1_40.scm
+++ chapter_1/section_1_3/exercise_1_40.scm
@@ -1,2 +1,15 @@
 (add-load-path "./")
+(add-load-path "../../tool/")
 (load "newtons_method.scm")
+(load "square.scm")
+(load "cube.scm")
+
+(define (cubic a b c)
+  (lambda (x)
+    (+ (cube x)
+       (* a (square x))
+       (* b x)
+       c)))
+
+(newtons-method (cubic 2 3 4) 1)
+                                        ;-1.6506291914330982
