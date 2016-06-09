f0fdf0a5c705d5639b189d85be45ae1fff3ce5ce
yongyang007 <yongyang007@gmail.com>
2016-06-09 10:32:02 +0900

    add exercise 2.50

---
+++ chapter_2/section_2_2/exercise_2_50_test.scm
@@ -0,0 +1,7 @@
+(add-load-path "./")
+(load "exercise_2_49_test.scm")
+(load "exercise_2_50.scm")
+
+;(draw (lambda () ((flip-horiz wave) frame)))
+;(draw (lambda () ((rotate180 wave) frame)))
+(draw (lambda () ((rotate270 wave) frame)))
