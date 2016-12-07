174df5193870a830eac8cd599779e90bc4aaefd7
yongyang007 <yongyang007@gmail.com>
2016-12-07 17:51:49 +0900

    add exercise 4.7 4.8

---
+++ chapter_4/section_1_1/exercise_4_7.scm
@@ -0,0 +1,29 @@
+(define (make-let bindings body)
+  (list 'let bindings body))
+
+(define (let*? exp) (tagged-list? exp 'let*))
+
+(define (let*-bindings exp) (cadr exp))
+(define (let*-body exp) (caddr exp))
+
+(define (let*->nested-lets exp)
+  (expand-body (let*-bindings exp) (let*-body exp)))
+
+(define (expand-body bindings body)
+  (if (null? bindings)
+      (make-let '() body)
+      (make-let (list (car bindings))
+                (if (null? (cdr bindings))
+                    body
+                    (expand-body (cdr bindings) body)))))
+
+;; test
+(define test-exp '(let* ((x 3) (y (+ x 2)) (z (+ x y 5))) (* x z)))
+(let*->nested-lets test-exp)
+;; => (let ((x 3)) (let ((y (+ x 2))) (let ((z (+ x y 5))) (* x z))))
+(let*->nested-lets '(let* () (+ 1 1)))
+;; => (let () (+ 1 1))
+
+;; 这样只要在eval方法的条件中加入下面的语句
+;; ((let*? exp) (eval (let*->nested-lets exp) evn))
+;; 就足够了，不必显式地以非派生的方法处理
---
+++ chapter_4/section_1_1/exercise_4_8.scm
@@ -0,0 +1,27 @@
+(add-load-path "./")
+(load "exercise_4_6.scm")
+
+(define (named-let? exp) (and (let? exp) (pair? (cdddr exp))))
+
+(define (let-var exp) (if (named-let? exp) (cadr exp) #f))
+(define (let-parameters exp) (map car ((if (named-let? exp) caddr cadr) exp)))
+(define (let-arguments exp) (map cadr ((if (named-let? exp) caddr cadr) exp)))
+(define (let-body exp) ((if (named-let? exp) cadddr caddr) exp))
+
+(define (make-definition variable value)
+  (list 'define variable value))
+
+(define (let->combination exp)
+  (let ((l (make-lambda
+            (let-parameters exp)
+            (let-body exp)))
+        (v (let-var exp))
+        (a (let-arguments exp)))
+    (if v
+        (make-begin (list (make-definition v l) (cons v a)))
+        (cons l a))))
+
+(let->combination test-exp)
+(define test-exp1 '(let <var> ((<var1> <exp1>) (<var2> <exp2>)) <body>))
+(let->combination test-exp1)
+;; => (begin (define <var> (lambda (<var1> <var2>) . <body>)) (<var> <exp1> <exp2>))
