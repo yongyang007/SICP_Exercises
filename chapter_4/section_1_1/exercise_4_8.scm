9238075e64748246925e044476f1293b7de62255
yongyang007 <yongyang007@gmail.com>
2016-12-14 16:10:39 +0900

    modify the part of let to support the named let by exercise 4.8

--- chapter_4/section_1_1/exercise_4_8.scm
+++ chapter_4/section_1_1/exercise_4_8.scm
@@ -1,12 +1,12 @@
 (add-load-path "./")
 (load "exercise_4_6.scm")
 
-(define (named-let? exp) (and (let? exp) (pair? (cdddr exp))))
+(define (named-let? exp) (and (let? exp) (symbol? (cadr exp))))
 
 (define (let-var exp) (if (named-let? exp) (cadr exp) #f))
 (define (let-parameters exp) (map car ((if (named-let? exp) caddr cadr) exp)))
 (define (let-arguments exp) (map cadr ((if (named-let? exp) caddr cadr) exp)))
-(define (let-body exp) ((if (named-let? exp) cadddr caddr) exp))
+(define (let-body exp) ((if (named-let? exp) cdddr cddr) exp))
 
 (define (make-definition variable value)
   (list 'define variable value))
@@ -24,4 +24,4 @@
 (let->combination test-exp)
 (define test-exp1 '(let <var> ((<var1> <exp1>) (<var2> <exp2>)) <body>))
 (let->combination test-exp1)
-;; => (begin (define <var> (lambda (<var1> <var2>) . <body>)) (<var> <exp1> <exp2>))
+;; => (begin (define <var> (lambda (<var1> <var2>) <body>)) (<var> <exp1> <exp2>))
--- chapter_4/section_1_1/metacircular_evaluator.scm
+++ chapter_4/section_1_1/metacircular_evaluator.scm
@@ -351,18 +351,28 @@
                'true
                (expand-or-clauses (cdr clauses)))))
 
-;; let
+;; modify the part of let to support the named letB
 (define (let? exp) (tagged-list? exp 'let))
 
-(define (let-parameters exp) (map car (cadr exp)))
-(define (let-arguments exp) (map cadr (cadr exp)))
-(define (let-body exp) (cddr exp))
+(define (named-let? exp) (and (let? exp) (symbol? (cadr exp))))
+
+(define (let-var exp) (if (named-let? exp) (cadr exp) #f))
+(define (let-parameters exp) (map car ((if (named-let? exp) caddr cadr) exp)))
+(define (let-arguments exp) (map cadr ((if (named-let? exp) caddr cadr) exp)))
+(define (let-body exp) ((if (named-let? exp) cdddr cddr) exp))
+
+(define (make-definition variable value)
+  (list 'define variable value))
 
 (define (let->combination exp)
-  (cons (make-lambda
-         (let-parameters exp)
-         (let-body exp))
-        (let-arguments exp)))
+  (let ((l (make-lambda
+            (let-parameters exp)
+            (let-body exp)))
+        (v (let-var exp))
+        (a (let-arguments exp)))
+    (if v
+        (make-begin (list (make-definition v l) (cons v a)))
+        (cons l a))))
 
 ;; let*
 (define (make-let bindings body)
