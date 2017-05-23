bbb8390b136c064cd3cd560df66552d314776e9c
yongyang007 <yongyang007@gmail.com>
2017-05-23 09:58:39 +0900

    change the format of comment

--- chapter_2/section_2_2/exercise_2_43.scm
+++ chapter_2/section_2_2/exercise_2_43.scm
@@ -4,7 +4,8 @@
           (adjoin-position new-row k rest-of-queens))
         (queen-cols (-k 1))))
  (enumerate-interval 1 board-size))
-                                        ;这种嵌套映射的顺序很明显的增加了递归的次数
-                                        ;相比于比效率高的过程在计算(queen-cols k)时
-                                        ;多调用了board-size倍的(queen-cols (- k 1))
-                                        ;时间大约是board-size^(board-size-1)倍
+
+;;这种嵌套映射的顺序很明显的增加了递归的次数
+;;相比于比效率高的过程在计算(queen-cols k)时
+;;多调用了board-size倍的(queen-cols (- k 1))
+;;时间大约是board-size^(board-size-1)倍
